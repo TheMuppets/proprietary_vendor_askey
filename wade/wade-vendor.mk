f0defae693595e66aa4fe67ce70b5acb72845d77
Aaron Kling <webgeek1234@gmail.com>
2021-07-01 14:37:31 -0500

    g12*: Decommonize TA files
    * These are different on all Askey G12*/SM1 devices.
    
    Change-Id: I4f798d094aacb4270619609c8cf9e0427abf2a95

--- wade/wade-vendor.mk
+++ wade/wade-vendor.mk
@@ -5,3 +5,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/askey/wade
 
+PRODUCT_COPY_FILES += \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+    vendor/askey/wade/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta
